0f33a1f3cb54e5d1fee3c14164a7f46b1156d313
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-03-26 09:01:23 +0100

    import data to the index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
+// React
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// Styles
 import "normalize.css"
 import './css/styles.css';
+
+// Components
 import App from './App';
 
+// Data
+import { names } from './data';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
