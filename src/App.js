779c22079940fdb5370d2138832544541b7dfbfc
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-03-26 11:29:21 +0100

    search component created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 // React
-import React from "react";
+import React, { Fragment } from "react";
 
 // Components
 import NamePicker from "./components/name-picker";
+import Search from "./components/search";
 
 
 function App({ names }) {
   return (
-    <NamePicker names={names} />
+    <Fragment>
+      <Search />
+      <NamePicker names={names} />
+    </Fragment>
   );
 }
 
---
+++ src/components/search.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Search() {
+    return(
+        <header>
+            <input type="text" placeholder="Type a name ..."></input>
+        </header>
+    )
+}
+
+export default Search;
