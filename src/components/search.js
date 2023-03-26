ad16bf7be036b8d51090cdc700238bac3fe335de
xiaozhao1111 <zhaoxiao20071106@gmail.com>
2023-03-26 11:36:41 +0100

    useState hook created for search component

--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Search() {
+    const [searchName, setSearchName] =useState('Simon');
+    function handleChange(event) {
+        setSearchName(event.target.value)
+    }
     return(
         <header>
-            <input type="text" placeholder="Type a name ..."></input>
+            <input 
+                type="text" 
+                placeholder="Type a name ..."
+                value={searchName}
+                onChange={handleChange}
+            />
         </header>
     )
 }
